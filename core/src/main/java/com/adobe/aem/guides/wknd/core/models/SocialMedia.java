f0c14d9126eebaded7771a2fa97fae4dc707fa28
a.a.pligina <a.a.pligina@gmail.com>
2023-03-24 19:48:46 +0300

    create custom share component

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/SocialMedia.java
@@ -0,0 +1,35 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.inject.Named;
+
+@Model(adaptables = {Resource.class},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class SocialMedia {
+    @ValueMapValue
+    @Named("socialMediaName")
+    private String name;
+    @ValueMapValue
+    @Named("socialMediaLink")
+    private String link;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/ShareModelImpl.java
@@ -0,0 +1,38 @@
+package com.adobe.aem.guides.wknd.core.models.impl;
+
+import com.adobe.aem.guides.wknd.core.models.Book;
+import com.adobe.aem.guides.wknd.core.models.BookItems;
+import com.adobe.aem.guides.wknd.core.models.ShareModel;
+import com.adobe.aem.guides.wknd.core.models.SocialMedia;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Model(adaptables = {SlingHttpServletRequest.class},
+        adapters = {ShareModel.class},
+        resourceType = {ShareModelImpl.RESOURCE_TYPE},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class ShareModelImpl implements ShareModel {
+    protected static final String RESOURCE_TYPE = "wknd/components/sharecomponent";
+
+    @ValueMapValue
+    private String title;
+    @Inject
+    @Via("resource")
+    private List<SocialMedia> resources;
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public List<SocialMedia> getResources() {
+        return resources;
+    }
+}
